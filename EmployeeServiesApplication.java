bd6f16baaf8cde6c7727799c980f2c9000f6608d
Vijaysingh <53387123+vijaySingh-king@users.noreply.github.com>
2024-04-01 17:22:20 +0530

    main class in Employee starter

---
+++ EmployeeServiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeServiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeServiesApplication.class, args);
+	}
+
+}
